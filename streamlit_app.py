9befdb40b83a8fce2c420bffbf5676a1408bc508
unknown <brave.fsj@gmail.com>
2024-07-24 12:49:05 +0800

    修改主页作者显示

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,9 @@ from io import BytesIO
 
 st.title('第七届职工职业技能竞赛大数据处理与审计初赛题目')
 st.header('—审计整改台账分析')
+st.write('###### by 智慧能源-18503')
 st.write('---')
-st.write('###### zhny18503')
+
 st.sidebar.title('导航')
 
 # 指定数据包文件夹路径
